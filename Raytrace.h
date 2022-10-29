e0b52259cc917971380f121623f324f55051ff02
Haoyan <657290301@qq.com>
2022-10-29 10:47:44 +0800

    part 1 finished
    10点47分 2022年10月29日

--- Raytrace.h
+++ Raytrace.h
@@ -12,7 +12,17 @@ const double pi = 3.1415926535897932385;
 inline double degree_to_radians(double degrees){
     return degrees * pi /180.0;
 }
-
+inline double random_double(){
+    return rand()/(RAND_MAX+1.0);
+}
+inline double random_double(double min,double max){
+    return min + (max-min)* random_double();
+}
+inline double clamp(double x,double min,double max){
+    if(x<min)return min;
+    if(x>max)return max;
+    return x;
+}
 #include "vec3.h"
 #include "ray.h"
 #endif
--- color.h
+++ color.h
@@ -4,12 +4,17 @@
 #include "vec3.h"
 
 #include <iostream>
+#include "Raytrace.h"
 
-void write_color(std::ostream &out, color pixel_color) {
+void write_color(std::ostream &out, color pixel_color,int sample_per_pixel) {
+    auto scale = 1.0/sample_per_pixel;
+    auto r = pixel_color.x()*scale;
+    auto g = pixel_color.y()*scale;
+    auto b = pixel_color.z()*scale;
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    out << static_cast<int>(256 * clamp(r,0.0,0.999)) << ' '
+        << static_cast<int>(256 * clamp(g,0.0,0.999)) << ' '
+        << static_cast<int>(256 * clamp(b,0.0,0.999)) << '\n';
 }
 
 #endif
