c99de9082ade218b3c38cbb8a5aa39e465449385
Haoyan <657290301@qq.com>
2022-10-30 22:24:12 +0800

    Final
    2022年10月30日22点24分

--- main.cpp
+++ main.cpp
@@ -6,16 +6,52 @@
 #include <iostream>
 #include "material.h"
 using namespace std;
-// Image
-const auto aspect_ratio = 16.0 / 9.0;
-const int image_width = 600;
-const int image_height = static_cast<int>(image_width / aspect_ratio);
-const int sample_per_pixel = 100;
-const int max_depth = 50;
-//camera
-camera cam;
-//World
-hittable_list world;
+
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
 
 color ray_color(const ray& r, const hittable& world,int depth) {//the color of ray direction's screen pixel
     if(depth<=0){
@@ -46,18 +82,23 @@ color ray_color(const ray& r, const hittable& world,int depth) {//the color of r
 }
 
 int main() {
-    auto material_behind = make_shared<lambertian>(color(0.8, 0.6, 0.2));
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 1280;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
-    world.add(make_shared<sphere>(point3( 0.0,    0.0,  1.1),   1.0, material_behind));
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    // World
+    auto world = random_scene();
+    
+    // Camera
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // Render
     cout << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -66,14 +107,14 @@ int main() {
         cerr << "\rcurrent: " <<100.f*(image_height-j)/(image_height)<<"%"<< flush;
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0,0,0);
-            for (int s = 0; s < sample_per_pixel; s++)
+            for (int s = 0; s < samples_per_pixel; s++)
             {
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
                 ray r = cam.get_ray(u,v);
                 pixel_color += ray_color(r,world,max_depth);
             }
-            write_color(cout, pixel_color,sample_per_pixel);
+            write_color(cout, pixel_color,samples_per_pixel);
         }
     }
 
