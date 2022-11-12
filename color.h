fab886165fc57e3a096e5b492d053d48332c0406
IMHaoyan <657290301@qq.com>
2022-11-12 16:17:02 +0800

    final

--- color.h
+++ color.h
@@ -9,6 +9,9 @@ void write_color(std::ostream &out, color pixel_color,int sample_per_pixel) {
     auto r = sqrt(pixel_color.x()*scale);
     auto g = sqrt(pixel_color.y()*scale);
     auto b = sqrt(pixel_color.z()*scale);
+    if (r != r) r = 0.0;
+    if (g != g) g = 0.0;
+    if (b != b) b = 0.0;
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(256 * clamp(r,0.0,0.999)) << ' '
         << static_cast<int>(256 * clamp(g,0.0,0.999)) << ' '
